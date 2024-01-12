ba5151967ac38cf11c5ffde02a205f445d69a763
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2024-01-11 19:28:38 -0500

    add comments

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,13 @@
+// Middleware function to check if a user is authenticated
 const withAuth = (req, res, next) => {
+  // If there is no user ID in the session, return a 401 Unauthorized status and redirect to login
   if (!req.session.user_id) {
-      return res.status(401).redirect('/login');
+    return res.status(401).redirect('/login');
   } else {
-      next();
+    // If there is a user ID in the session, call the next middleware or route handler
+    next();
   }
 };
 
+// Export the withAuth middleware for use in other parts of the application
 module.exports = withAuth;
